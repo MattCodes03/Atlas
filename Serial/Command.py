aa01880020330dab1dd491d2ee8a64aa5349f135
Matthew <matthewmccann41@gmail.com>
2023-06-22 14:27:34 +0100

    Added Command Class

--- Serial/Command.py
+++ Serial/Command.py
@@ -4,6 +4,8 @@ class Command:
     def __init__(self, signal: str, args: str):
         self.__setup(signal, args)
 
+    def __str__(self) -> str:
+        return str(self.command)
+
     def __setup(self, signal, args):
-        self.signal = signal
-        self.args = args
+        self.command = {'command': signal, 'args': args}
