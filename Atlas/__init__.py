0180c0998180f153ec52995be236e855ac66b8dc
Matthew <matthewmccann41@gmail.com>
2023-06-29 23:09:05 +0100

    Updated __init__.py

--- Atlas/__init__.py
+++ Atlas/__init__.py
@@ -1,11 +1,15 @@
 # NOTE: When running locally use pip install -e .
 
-from Atlas.Controllers.Servo import Servo
-from Atlas.Controllers.LED import LED
-from Atlas.Controllers.DCMotor import DCMotor, FORWARD, BACKWARD
-from Atlas.Serial.Receiver import Receiver
-from Atlas.Serial.Command import Command
-from Atlas.Serial.SerialConnection import Serial
+
+from sys import platform
+if platform == "linux" or platform == "linux2":
+    from Atlas.Serial.Command import Command
+    from Atlas.Serial.SerialConnection import Serial
+else:
+    from Atlas.Controllers.Servo import Servo
+    from Atlas.Controllers.LED import LED
+    from Atlas.Controllers.DCMotor import DCMotor, FORWARD, BACKWARD
+    from Atlas.Serial.Receiver import Receiver
 
 
 __all__ = ["Serial", "Command", "Receiver", "DCMotor",
