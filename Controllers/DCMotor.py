51d2f665ab1039fa977a2c877826eca9c59e8aa0
Matthew <matthewmccann41@gmail.com>
2023-06-29 20:26:32 +0100

    Updated Direction Constants

--- Controllers/DCMotor.py
+++ Controllers/DCMotor.py
@@ -1,26 +1,26 @@
 from machine import Pin, PWM
 
+# Directions
+FORWARD = 0
+BACKWARD = 1
+
 
 class DCMotor:
     __pwm_frequency: int = 2000
     __min_u16_duty: int = 750
     __max_u16_duty: int = 1023
 
-    # Directions
-    FORWARD = 0
-    BACKWARD = 1
-
     def __init__(self, in1: int, in2: int, en: int):
         self.__setup(in1, in2, en)
 
     def move(self, speed, direction):
-        if direction == self.FORWARD:
+        if direction == FORWARD:
             print("Forward...")
             self.input1.high()
             self.input2.low()
             self.enable_pin.duty_u16(self.__calculate_u16_duty(speed))
 
-        elif direction == self.BACKWARD:
+        elif direction == BACKWARD:
             print("Backward...")
             self.input1.low()
             self.input2.high()
