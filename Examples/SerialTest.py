67e388c629bc8bf26ea1fe5edb899ad1775b912b
Matthew <matthewmccann41@gmail.com>
2023-06-29 20:56:14 +0100

    Updated Serial Example

--- Examples/SerialTest.py
+++ Examples/SerialTest.py
@@ -1,12 +1,18 @@
-from Serial import Serial
-from Command import Command
+from Serial.UART import UART
+from Serial.Command import Command
+import Controllers.DCMotor as dcm
 import json
 
-serial = Serial('/dev/serial0')
+serial = UART('/dev/serial0')
 
 while True:
     speed = input("What Speed: ")
     direction = input("What Direction: ")
+
+    if direction == 'f':
+        direction = dcm.FORWARD
+    else:
+        direction = dcm.BACKWARD
 
     command = Command('MOTOR', f'{speed},{direction}')
 
